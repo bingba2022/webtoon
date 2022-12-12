aa5e2e31778f138057efc6fc4a3cd605040def70
bingba2022 <bingbakim@gmail.com>
2022-12-12 12:56:29 +0900

    First Commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 1.3,
+      ),
+    );
+  }
+}
---
+++ lib/services/api_service.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    await http.get(url);
+  }
+}
