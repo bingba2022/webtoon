7665bd1cb42f629d1b058da064eed04b7bce9bf5
bingba2022 <bingbakim@gmail.com>
2022-12-14 16:18:43 +0900

    added webtoon image and shadow

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -29,15 +29,15 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           // snapshot: state of your future
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                print(index);
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
             );
           }
           return const Center(
@@ -45,6 +45,51 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        print(index);
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15, // how far away the shadows will be
+                    offset: const Offset(
+                        10, 10), // where is the shadow located (0,0) -> center
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(webtoon.thumb),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
   }
 }
