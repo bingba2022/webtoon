d04c43fdc15d4a7939b764f9cf9745d577d43ffc
bingba2022 <bingbakim@gmail.com>
2022-12-14 17:10:58 +0900

    created navigator push for different screen

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        elevation: 1.3,
+      ),
+    );
+  }
+}
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -11,34 +12,50 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 15, // how far away the shadows will be
-                offset: const Offset(
-                    10, 10), // where is the shadow located (0,0) -> center
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ],
-            borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      // detect gestures
+      onTap: () {
+        Navigator.push(
+          // make the user think we are going to different screen
+          context,
+          MaterialPageRoute(
+            builder: ((context) => DetailScreen(
+                  title: title,
+                  thumb: thumb,
+                  id: id,
+                )),
           ),
-          width: 250,
-          clipBehavior: Clip.hardEdge,
-          child: Image.network(thumb),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22,
+        );
+      },
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 15, // how far away the shadows will be
+                  offset: const Offset(
+                      10, 10), // where is the shadow located (0,0) -> center
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            width: 250,
+            clipBehavior: Clip.hardEdge,
+            child: Image.network(thumb),
           ),
-        ),
-      ],
+          const SizedBox(
+            height: 15,
+          ),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
