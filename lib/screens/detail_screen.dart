1f2205fa303b445b37a4fa4659d549287dfc6045
bingba2022 <bingbakim@gmail.com>
2022-12-14 17:38:57 +0900

    added Hero

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -26,6 +26,37 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 1.3,
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: id,
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15, // how far away the shadows will be
+                        offset: const Offset(10,
+                            10), // where is the shadow located (0,0) -> center
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.network(thumb),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -24,26 +24,30 @@ class Webtoon extends StatelessWidget {
                   thumb: thumb,
                   id: id,
                 )),
+            fullscreenDialog: true, // pops up from the top
           ),
         );
       },
       child: Column(
         children: [
-          Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15, // how far away the shadows will be
-                  offset: const Offset(
-                      10, 10), // where is the shadow located (0,0) -> center
-                  color: Colors.black.withOpacity(0.5),
-                ),
-              ],
-              borderRadius: BorderRadius.circular(10),
+          Hero(
+            tag: id,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15, // how far away the shadows will be
+                    offset: const Offset(
+                        10, 10), // where is the shadow located (0,0) -> center
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(thumb),
             ),
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            child: Image.network(thumb),
           ),
           const SizedBox(
             height: 15,
